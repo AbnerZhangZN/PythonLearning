1866cd86436fc8efa4eb42c772b024f150a8eb11
whzhang1 <znzhangwenhao@126.com>
2017-12-13 18:09:40 +0800

    20171213 whzhang1 function

---
+++ function/day002_05.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- 
+#setup 8 函数-基础函数和函数调用
+print(abs(-12))#绝对值
+print(max(1,2,3,44,5))#最大值
+print(int('123'))
+#print(int('122.33'))#error
+print(int(122.33))
+print(str(233))
+print(bool(''))
+print(bool(1))
+print(bool(0))
+a = abs# 变量a指向abs函数,所以也可以通过a调用abs函数
+print(a(-233))
---
+++ function/day002_06.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- 
+#setup 8 函数-自定义函数
+def my_abs(x):
+	if x>0:
+		return x
+	else:
+		return -x
+
+print(my_abs(-12))
+
+def nop():
+	pass
+
+nop()
+
+import math
+
+def move(x,y,step,angle=0):
+	nx = x + step*math.cos(angle)
+	ny = y + step*math.sin(angle)
+	return nx,ny
+
+print(move(3,4,3,30))#ython的函数返回多值其实就是返回一个tuple
+
+def quadratic(a,b,c):
+	if 
