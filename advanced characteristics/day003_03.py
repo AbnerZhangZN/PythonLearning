63dad237e4b7836c07cab507bda65db81706b683
whzhang1 <znzhangwenhao@126.com>
2017-12-15 11:17:27 +0800

    20171215 whzhang1 高级特性-切片

--- advanced characteristics/day003_03.py
+++ advanced characteristics/day003_03.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*- 
 #setup 12 高级特性-切片
 #L[0:3]表示，从索引0开始取，直到索引3为止，但不包括索引3。即索引0，1，2，正好是3个元素。
+#用法:
+#L[n1:n2:n3]
+#n1代表开始元素下标
+#n2代表结束元素下标
+#n3代表切片间隔以及切片方向,默认值是1
+#L中每个元素都有正负两种下标，例如L[0]和L[-10]指的同一个元素都是0
 L=['Michael','Sarah','Jackie','Bob','Tracy','Kobe']
 print([L[0],L[1],L[2]])
 print(L[0:3])
@@ -30,4 +36,4 @@ def trim(s):
 		s = s[:len(s)-1]
 	return s
 print(trim('   asdf   '))
-print(len(trim('   asds   ssf   ')))
+print(trim('   asds   ssf   '))
