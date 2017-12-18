08b7decf221c9a7e3d2e57eb9ddd4643e733d7e2
whzhang1 <znzhangwenhao@126.com>
2017-12-18 10:54:40 +0800

    20171218 whzhang1 目录整理删除文件

--- base/day001_00.py
+++
@@ -1,6 +0,0 @@
-print('计算客户的相关金额')
-exporeSum = input('客户的敞口:')
-duebillSum = input('客户的余额:')
-bailSum = input('客户的保证金:')
-result = exporeSum - duebillSum + bailSum
-print('客户的剩余敞口(敞口-余额+保证金):', result)
--- base/day001_01.py
+++
@@ -1,9 +0,0 @@
-#setup 1 练习-基础
-
-#输入变量赋值
-name = input('please input your name:')
-#输出变量
-print('hello', name)
-
-#退出
-exit()
